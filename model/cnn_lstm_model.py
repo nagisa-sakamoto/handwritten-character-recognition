2c1c34d9ad4232bcd5c6788b42feb1b137ce3305
ngisa.sakamoto <nagisa.sakamoto@linecorp.com>
2020-08-23 07:26:44 +0900

    Add BatchNorm layers

--- model/cnn_lstm_model.py
+++ model/cnn_lstm_model.py
@@ -13,15 +13,19 @@ class CnnLstmModel(nn.Module):
         self.num_rnn_layers = num_rnn_layers
         self.conv = nn.Sequential(
                         nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=0),
+                        nn.BatchNorm2d(32),
                         nn.ReLU(inplace=True),
                         nn.MaxPool2d(kernel_size=2, stride=2),
                         nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=0),
+                        nn.BatchNorm2d(32),
                         nn.ReLU(inplace=True),
                         nn.MaxPool2d(kernel_size=2, stride=2),
                         nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=0),
+                        nn.BatchNorm2d(64),
                         nn.ReLU(inplace=True),
                         nn.MaxPool2d(kernel_size=2, stride=2),
                         nn.Conv2d(64, 64, kernel_size=5, stride=1, padding=0),
+                        nn.BatchNorm2d(64),
                         nn.ReLU(inplace=True),
                         nn.MaxPool2d(kernel_size=2, stride=2)
         )
@@ -46,13 +50,15 @@ class CnnLstmModel(nn.Module):
             target_seq += self.targets[i]
         return target_seq
     
-    def forward(self, x):
+    def forward(self, x, input_lengths):
         x.unsqueeze_(2)
         batch_size, seq_length, C, H, W = x.size()
         x = x.view(batch_size * seq_length, C, H, W) # batchN*frameN x C x H x W
         x = self.conv(x)
         x = x.view(batch_size, seq_length, -1) # batchN x frameN x featureDim
+        x = nn.utils.rnn.pack_padded_sequence(x, input_lengths, batch_first=True)
         x, _ = self.rnn(x)
+        x, _ = nn.utils.rnn.pad_packed_sequence(x, batch_first=True)
         y = self.fc(x)
         return y
 
